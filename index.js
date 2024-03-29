4cfdea3bfe74c8055bc69b295697dbd17915e1a8
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-03-29 14:14:00 +0100

    Version 2.1

--- index.js
+++ index.js
@@ -8,13 +8,7 @@
 //   dueDate: '2022-12-22'
 // }];
 
-const todoList = JSON.parse(localStorage.getItem('todoList')) || [{
-  name: 'make dinner',
-  dueDate: '2022-12-22'
-}, {
-  name: 'wash dishes',
-  dueDate: '2022-12-22'
-}];
+const todoList = JSON.parse(localStorage.getItem('todoList')) || [{}];
 
 renderTodoList();
 
