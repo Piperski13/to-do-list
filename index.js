c9ebbe3535b2a50abcd8d6cbfc70a46c119e8e33
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-04-15 16:09:47 +0200

    Version 3.6.1

--- index.js
+++ index.js
@@ -88,36 +88,13 @@ document.querySelectorAll('.done')
 localStorage.setItem('todoList',JSON.stringify(todoList)); // seting object to a string
   
 };
-// We use forEach insted of for loop
-
-// for (let i = 0; i < todoList.length; i++) {
-//   const todoObject = todoList[i];
-
-//   // const name = todoObject.name;
-//   // const dueDate = todoObject.dueDate;
-//   const {name,dueDate} = todoObject; // Destructor, same as the comented code above
-
-//   const html = `
-//   <div>${name}</div> 
-//   <div>${dueDate}</div>
-//   <button onclick="
-//   todoList.splice(${i},1);
-//   renderTodoList();
-//   " class="delete-todo-button">Delete</button>  
-//   `;
-//   todoListHTML += html;
-// }
-// document.querySelector('.js-todo-list').innerHTML = todoListHTML;
-
-// localStorage.setItem('todoList',JSON.stringify(todoList)); // seting object to a string
-// }
-
 
 const addTodoElement = document.querySelector('.js-add-todo');
 addTodoElement.addEventListener('click',()=>{
   addTodo();
 })
 
+//gets name and date values and stores it localy and prints it on screen;
 function addTodo(){
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
